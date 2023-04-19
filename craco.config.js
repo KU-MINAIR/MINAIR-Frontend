c5c1680b9b1f6948a521975092ec3ebaca625b71
hhnn0 <hyun0520ho@gmail.com>
2023-04-20 02:29:45 +0900

    chore: Craco 설치

--- craco.config.js
+++ craco.config.js
@@ -1,16 +1,13 @@
-const path = require("path");
+const CracoAlias = require("craco-alias");
 
 module.exports = {
-  webpack: {
-    alias: {
-      "@": path.resolve(__dirname, "src/"),
-    },
-  },
-  jest: {
-    configure: {
-      moduleNameMapper: {
-        "^@(.*)$": "<rootDir>/src$1",
+  plugins: [
+    {
+      plugin: CracoAlias,
+      options: {
+        source: "tsconfig",
+        tsConfigPath: "tsconfig.paths.json",
       },
     },
-  },
+  ],
 };
--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App() {
   return (
     <>
       <GlobalStyle />
-      <div className="App"></div>;
+      <div className="App">aa</div>
     </>
   );
 }
